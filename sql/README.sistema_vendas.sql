8ce8321933f670ddc91550203f6728b7a0f8a568
Guilherme <guiguizaovial@gmail.com>
2026-03-20 12:58:02 -0300

    README.sistema_vendas.sql

---
+++ sql/README.sistema_vendas.sql
@@ -0,0 +1,17 @@
+#  Sistema de Banco de Dados - Vendas
+
+#  Descrição
+Projeto de banco de dados simulando um sistema de vendas com clientes, produtos e pedidos.
+
+# Conceitos aplicados
+- Modelagem relacional
+- PRIMARY KEY e FOREIGN KEY
+- INNER JOIN
+- GROUP BY
+- Funções de agregação
+
+# Consultas
+- Total gasto por cliente
+- Produto mais vendido
+- Faturamento total
+- Relatório de pedidos
