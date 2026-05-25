84d8513eb7deb562962e868e2000cffc9d6ad10c
Guilherme <guiguizaovial@gmail.com>
2026-05-25 13:16:43 -0300

    Conversor de Temperatura

---
+++ python/Conversor de Temperatura.py
@@ -0,0 +1,7 @@
+print("=== CONVERSOR DE TEMPERATURA ===")
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperatura em Fahrenheit:", fahrenheit)
