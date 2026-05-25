b7ff3a0146743f1ecd8c5036beb28ab057aadcd9
Guilherme <guiguizaovial@gmail.com>
2026-05-25 13:17:26 -0300

    Verificador de numero primo

---
+++ python/Verificação de numero primo.py
@@ -0,0 +1,17 @@
+numero = int(input("Digite um número: "))
+
+primo = True
+
+if numero <= 1:
+    primo = False
+
+for i in range(2, numero):
+
+    if numero % i == 0:
+        primo = False
+
+if primo:
+    print("É número primo")
+
+else:
+    print("Não é número primo")
