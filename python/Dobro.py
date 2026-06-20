12e68b5a6cccb6acf403f52f412c07304f5fc0e1
Guilherme <guiguizaovial@gmail.com>
2026-06-20 10:59:54 -0300

    Calculando o dobro usando def

---
+++ python/Dobro.py
@@ -0,0 +1,9 @@
+def calcular_dobro(conta):
+    resultado = conta * 2
+    return resultado
+
+conta = int(input("Digite um numero:"))
+
+dobro = calcular_dobro(conta)
+
+print("O dobro do numero é:", dobro )
